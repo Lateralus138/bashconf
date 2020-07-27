9ba6f8d951a08a1d5d30db34d902664ad7d65c96
Lateraus138 <faithnomoread@yahoo.com>
2020-07-26 19:32:12 -0500

    Fixed bugs, finished README, ready for release.

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 chmod +x ./appimagetool-x86_64.AppImage
-./appimagetool-x86_64.AppImage appimage/bashconf.AppDir dist/bashconf-x86_64.AppImage
+./appimagetool-x86_64.AppImage appimage/bashconf.AppDir
+touch nf
